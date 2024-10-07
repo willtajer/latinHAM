0661ac712caded3b79869f02806c2897e8aa0949
William Anderson <knahtuoy@13.local>
2024-10-07 11:07:34 -0600

    adjuted willtajer link

--- components/LatinHamGame.tsx
+++ components/LatinHamGame.tsx
@@ -227,7 +227,7 @@ const LatinHamGame: React.FC = () => {
           <DifficultySelector onSelectDifficulty={handleDifficultySelect} />
         </div>
         <a
-          href="https://willtajer.com/"
+          href="https://willtajer.com/projects/latinham/"
           target="_blank"
           rel="noopener noreferrer"
           className="fixed bottom-4 right-4 bg-yellow-400 hover:bg-yellow-500 text-black font-bold py-2 px-4 rounded-full shadow-lg transition-colors duration-200 focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:ring-opacity-50"
