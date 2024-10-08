9fa984f5853465a661119b715acac8b84bd18a82
William Anderson <knahtuoy@13.local>
2024-10-08 00:53:17 -0600

    testing <3

--- components/DiscoveredLatinHAMs.tsx
+++ components/DiscoveredLatinHAMs.tsx
@@ -70,7 +70,6 @@ export const DiscoveredLatinHAMs: React.FC = () => {
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-6">Discovered LatinHAMs</h1>
       <LatinHAMGrid 
         latinHAMs={latinHAMs} 
         onLatinHAMClick={handleLatinHAMClick}
