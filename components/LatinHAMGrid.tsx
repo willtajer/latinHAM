bc90a1b76efba9a299e3b3d74d3d57d5b41a168b
William Anderson <knahtuoy@13.lan>
2024-10-08 11:43:21 -0600

    updated discovered

--- components/LatinHAMGrid.tsx
+++ components/LatinHAMGrid.tsx
@@ -93,7 +93,7 @@ const LatinHAMGrid: React.FC<LatinHAMGridProps> = ({
 
   return (
     <>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4 justify-items-center">
+      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4 justify-items-center">
         {latinHAMs.map((latinHAM, index) => (
           <div 
             key={`latinHAM-${index}`} 
