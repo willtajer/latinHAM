88c40c92fe5936c3bef7253f2d2216a8aa58dc6a
William Anderson <knahtuoy@13.local>
2024-10-09 00:09:37 -0600

    adjusted discover page top padding

--- app/discovered/page.tsx
+++ app/discovered/page.tsx
@@ -7,7 +7,7 @@ import LearningModeButton from '@/components/LearningModeButton'
 
 export default function DiscoveredPage() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-transparent text-foreground">
+    <div className="flex flex-col items-center min-h-screen bg-transparent text-foreground">
       <div className="w-[calc(6*3rem+6*0.75rem)]">
         <HomeButton />
           <h1 className="text-6xl font-bold mt-6 mb-6 text-center">latinHAM</h1>
