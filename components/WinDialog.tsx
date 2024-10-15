b0e2d7694a1d570227be3797b8ea39feb57b42a9
William Anderson <knahtuoy@13.local>
2024-10-15 12:46:19 -0600

    lint

--- components/WinDialog.tsx
+++ components/WinDialog.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter } from '@/components/ui/dialog'
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { CompletedPuzzleCard } from './CompletedPuzzleCard'
